a466f068bb944c135f83ff19af2602151089424a
RobinBobo <robin.bobo@laposte.net>
2014-11-30 19:09:09 +0100

    Ajout des beans Categorie, Commande et Panier.

---
+++ ProjetTIC/src/JavaBeans/Categorie.java
@@ -0,0 +1,27 @@
+package JavaBeans;
+
+public class Categorie {
+	
+	private int idCategorie;
+	private String nomCategorie;
+	
+	public Categorie () {
+		
+	}
+
+	public int getIdCategorie() {
+		return idCategorie;
+	}
+
+	public void setIdCategorie(int idCategorie) {
+		this.idCategorie = idCategorie;
+	}
+
+	public String getNomCategorie() {
+		return nomCategorie;
+	}
+
+	public void setNomCategorie(String nomCategorie) {
+		this.nomCategorie = nomCategorie;
+	}
+}
---
+++ ProjetTIC/src/JavaBeans/Panier.java
@@ -0,0 +1,18 @@
+package JavaBeans;
+
+public class Panier {
+	
+	private int idPanier;
+	
+	public Panier () {		
+	}
+
+	public int getIdPanier() {
+		return idPanier;
+	}
+
+	public void setIdPanier(int idPanier) {
+		this.idPanier = idPanier;
+	}
+
+}
