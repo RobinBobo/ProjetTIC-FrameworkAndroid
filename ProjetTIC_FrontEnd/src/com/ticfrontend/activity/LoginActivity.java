d5883fab00f6b9a71634a2a991c1490f3f3bef77
florian2412 <florian2412@gmail.com>
2015-02-24 20:57:42 +0100

    Ajout écran login

--- ProjetTIC_FrontEnd/src/com/ticfrontend/activity/LoginActivity.java
+++ ProjetTIC_FrontEnd/src/com/ticfrontend/activity/LoginActivity.java
@@ -9,19 +9,46 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class LoginActivity extends Activity {
 
+	public static boolean ISCONNECTED = false;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		// Connexion de l'utilisateur
+		// Puis changement des boutons de l'affichage principal
+		
+		Button validerLogin = (Button) findViewById(R.id.boutonValiderLogin);
+		validerLogin.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				EditText login = (EditText) findViewById(R.id.editTextLoginUser);
+				EditText password = (EditText) findViewById(R.id.editTextPassUser);
+				if(login.getText().toString().length() > 0 && password.getText().toString().length() > 0){
+					ISCONNECTED = true;	
+					onBackPressed();				
+				}
+			}
+		});
+		
+		
+		
+		
+		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
 
@@ -30,10 +57,7 @@ public class LoginActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
+
 		return super.onOptionsItemSelected(item);
 	}
 }
